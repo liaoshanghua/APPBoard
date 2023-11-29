3376733d414578d0b64bc4b56d1b4e2a928cf0e8
MSI\phone <phone8@outlook.com>
2023-11-29 11:46:03 +0800

    首次提交

---
+++ App.vue
@@ -0,0 +1,34 @@
+<script>
+	export default {
+		onLaunch: function() {
+			// #ifdef APP-PLUS
+			// 锁定屏幕方向 横屏正方向
+			plus.screen.lockOrientation('landscape-primary');
+			// #endif
+		},
+		onShow: function() {
+			// #ifdef APP-PLUS
+			// 锁定屏幕方向 横屏正方向
+			plus.screen.lockOrientation('landscape-primary');
+			// #endif
+		},
+		onHide: function() {
+			// #ifdef APP-PLUS
+			// 锁定屏幕方向 横屏正方向
+			plus.screen.lockOrientation('landscape-primary');
+			// #endif
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+
+	uni-page-head {
+		display: none;
+	}
+
+	uni-page-wrapper {
+		height: 100% !important;
+	}
+</style>
